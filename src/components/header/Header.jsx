288d005eccc92df46cf676f791dfa6795b0e9996
XiphiasSB <Isaksen85@gmail.com>
2024-12-15 10:01:08 +0100

    Added logo and improved CSS

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,11 @@
+import crown from '../../assets/crown.png'
+import './Header.css'
 
-
-export function Header(){
-    return(
-        <h1>The Princess To-Do List</h1>
-    )
+export function Header() {
+  return (
+    <header>
+      <img src={crown} alt="Princess crown" className="crown-logo" />
+      <h1 className="header-title">The Princess To-Do List</h1>
+    </header>
+  )
 }
